f8212b6e3a0f8412f0f54435885a50f1234fbe54
Clayton James Strickland <RdDvls@Clay.local>
2016-08-18 11:03:47 -0400

    base version

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,12 @@
+package tiyinc.noobs;
+
+/**
+ * Created by RdDvls on 8/18/16.
+ */
+public class Day9Runner {
+
+    public static void main(String[] args){
+        System.out.println("Running....");
+    }
+
+}
